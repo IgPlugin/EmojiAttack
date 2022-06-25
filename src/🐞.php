6b5942edffe12c47eccd8c610f640ab3ac24837c
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-06-25 21:52:16 +0700

    Config instead of hard code

--- src/🐞.php
+++ src/🐞.php
@@ -10,16 +10,21 @@ use pocketmine\event\server\DataPacketSendEvent as 🛳️;
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket as 📦;
 use pocketmine\network\mcpe\protocol\TextPacket as 💬;
 use pocketmine\utils\TextFormat as 🌈;
+use pocketmine\utils\Config as 📝;
 
 class 🐞 extends 🏠 implements 👂 {
 
 	public const ✈️ = 🌈::ESCAPE . "\u{3000}";
+	public const ✔️ = true;
+	public const ❌ = false;
 
-	public const 🛒 = ["ヾ(≧▽≦*)o", "φ(*￣0￣)", "q(≧▽≦q)", "ψ(｀∇´)ψ", "（￣︶￣）↗", "*^____^*", "(～￣▽￣)～", "( •̀ ω •́ )✧", "[]~(￣▽￣)~*", "φ(゜▽゜*)♪", "o(*^＠^*)o", "O(∩_∩)O", "(✿◡‿◡)", "`(*>﹏<*)′", "(*^▽^*)", "（*＾-＾*）", "(❁´◡`❁)", "(≧∇≦)ﾉ", "(´▽`ʃ♡ƪ)", "(●ˇ∀ˇ●)", "○( ＾皿＾)っ Hehehe…", "(￣y▽￣)╭ Ohohoho.....", "\^o^/", "(‾◡◝)", "╰(*°▽°*)╯", "(〃￣︶￣)人(￣︶￣〃)", "o(*^▽^*)┛", "o(*￣▽￣*)ブ", "(^_-)db(-_^)", "o(*￣▽￣*)ブ", "♪(^∇^*)", "(≧∀≦)ゞ", "o(*￣︶￣*)o", "--<-<-<@", "(oﾟvﾟ)ノ", "o(*≧▽≦)ツ┏━┓", "(/≧▽≦)/", "( $ _ $ )", "(☆▽☆)", "ヾ(＠⌒ー⌒＠)ノ", "ㄟ(≧◇≦)ㄏ", "o((>ω< ))o", "( *︾▽︾)", "ヾ(≧ ▽ ≦)ゝ", "☆*: .｡. o(≧▽≦)o .｡.:*☆", "(((o(*ﾟ▽ﾟ*)o)))", "＼(((￣(￣(￣▽￣)￣)￣)))／", "♪(´▽｀)", "( *^-^)ρ(^0^* )", "~~~///(^v^)\\\~~~", "(^///^)", "(p≧w≦q)", "o(*￣▽￣*)o", "( •̀ ω •́ )y", "(o゜▽゜)o☆", "ƪ(˘⌣˘)ʃ"];
+	protected 📝 $🛒;
 
 	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		/** Mom, Look! It works! 😱 */
+		$this->saveResource("🛒.txt");
+		$this->🛒 = new 📝($this->getDataFolder()."🛒.txt", 📝::ENUM);
 	}
 
 	public function 🚀(🛳️ $🎉): void {
@@ -34,7 +39,7 @@ class 🐞 extends 🏠 implements 👂 {
 						$✨->message = $this->🤔($✨->message);
 						break;
 					default:
-						$✨->message .= " " . self::🛒[array_rand(self::🛒)] . self::✈️;
+						$✨->message .= " " . $this->🛒->getAll(self::✔️)[array_rand($this->🛒->getAll(self::✔️))] . self::✈️;
 						break;
 				}
 			} elseif ($✨ instanceof 📦) {
@@ -46,6 +51,6 @@ class 🐞 extends 🏠 implements 👂 {
 	}
 
 	public function 🤔(string $📃): string {
-		return preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $📃) . " " . self::🛒[array_rand(self::🛒)] . self::✈️;
+		return preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $📃) . " " . $this->🛒->getAll(self::✔️)[array_rand($this->🛒->getAll(self::✔️))] . self::✈️;
 	}
 }
